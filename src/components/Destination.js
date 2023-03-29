18b3ea731746036d62023e3f9ab45bf014224a5d
mrhabibali <habibali92@hotmail.com>
2023-03-29 17:54:09 +0200

    Made a Destination component

---
+++ src/components/Destination.js
@@ -0,0 +1,7 @@
+function Destination() {
+  return <div className="content"></div>;
+}
+export default Destination;
+
+//<img src="https://source.unsplash.com/JmuyB_LibRo" alt="Australia" />
+//<img src="https://source.unsplash.com/3PeSjpLVtLg" alt="Norway" />
