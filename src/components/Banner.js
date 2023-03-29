066d8f6cc6605e88bab27b44d36dd93d822e085a
mrhabibali <habibali92@hotmail.com>
2023-03-29 17:49:34 +0200

    Added some css

---
+++ src/components/Banner.js
@@ -0,0 +1,14 @@
+function Banner() {
+  return (
+    <div className="banner">
+      <img
+        src="https://source.unsplash.com/vhSz50AaFAs"
+        className="banner--globe"
+        alt="globe"
+      />
+      <h3>My travel journal</h3>
+    </div>
+  );
+}
+
+export default Banner;
